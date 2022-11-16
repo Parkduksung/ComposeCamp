7fa5d8bd5ca7d352a6b41a3abd091e29869a0ae4
Parkduksung <duksung1234@naver.com>
2022-11-16 23:23:17 +0900

    [feature] unit1-path5 (1) TextAlign 이라는걸 추가해줘야 한다.

--- beginner/src/main/java/com/example/beginner/unit1/path5/Unit1Path5.kt
+++ beginner/src/main/java/com/example/beginner/unit1/path5/Unit1Path5.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,6 +45,7 @@ fun Article() {
 
         Text(
             text = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+            textAlign = TextAlign.Justify,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 16.dp, end = 16.dp)
@@ -52,6 +53,7 @@ fun Article() {
 
         Text(
             text = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app\\'s UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI\\'s construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+            textAlign = TextAlign.Justify,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
