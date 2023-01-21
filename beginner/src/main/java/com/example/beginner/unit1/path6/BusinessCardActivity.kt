0b2f7d736d40d0c32ed6f8dcf9086cd21aee48e2
Parkduksung <duksung1234@naver.com>
2023-01-21 21:41:56 +0900

    [feature] Unit2Path2.

---
+++ beginner/src/main/java/com/example/beginner/unit1/path6/BusinessCardActivity.kt
@@ -0,0 +1,17 @@
+package com.example.beginner.unit1.path6
+
+import androidx.compose.runtime.Composable
+import com.example.beginner.BaseActivity
+
+class BusinessCardActivity : BaseActivity() {
+
+
+    @Composable
+    override fun InitSetContent(vararg block: @Composable () -> Unit) {
+
+    }
+
+
+
+
+}
